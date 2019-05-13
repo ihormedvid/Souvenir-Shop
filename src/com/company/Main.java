829452e6833ef6931569e3abed96ab09ca99392f
Ihor Medvid <igormedvid89@gmail.com>
2019-05-14 01:10:08 +0300

    user data processing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.io.*;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String userString = bufferedReader.readLine();
+
+        if (userString.isEmpty()) {
+            System.out.println("Стрічка пуста");
+        }
+        String[] userStringSplited = userString.split("\\s+");
+        String userCommand = userStringSplited[0].toLowerCase();
+
+        switch (userCommand) {
+            case ("purchase"):
+                //TODO
+                break;
+            case ("all"):
+                //TODO
+                break;
+            case ("clear"):
+                //TODO
+                break;
+            case ("report"):
+                //TODO
+                break;
+        }
+
     }
+
 }
+
+
